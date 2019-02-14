73dc6ccb0d2f35dd28fd19685e1aa8f07dc422ec
tajiritomoyuki <constantinoplite@gmail.com>
2019-02-14 19:13:01 +0900

    quality

--- quality.py
+++ quality.py
@@ -22,40 +22,52 @@ def loadFFI(sector, camera, CCD):
     fitslist.sort()
     return fitslist
 
+def gather_hdf(sector):
+    h5list = glob.glob(os.path.join(step2, "*_%s_?_?.h5" % sector))
+    return h5list
+
+def fits2pos(fitslist):
+    x_center = np.array([])
+    y_center = np.array([])
+    print("camera%s chip%s..." % (came, chi))
+    for fitspath in tqdm(fitslist):
+        #x, y座標を格納
+        with fits.open(fitspath) as hdu:
+            x = hdu[1].header["CRVAL1"]
+            y = hdu[1].header["CRVAL2"]
+            x_center = np.hstack((x_center, x))
+            y_center = np.hstack((y_center, y))
+    return x_center, y_center
+
+def pos2quality(x_center, y_center, sigma):
+    #統計値を算出
+    x_mean = np.nanmean(x_center)
+    y_mean = np.nanmean(y_center)
+    x_std = np.nanstd(x_center)
+    y_std = np.nanstd(y_center)
+    #sigma以上離れている点を1とする
+    x_cond = np.logical_or(x_center > x_mean + sigma * x_std, x_center < x_mean - sigma * x_std)
+    y_cond = np.logical_or(y_center > y_mean + sigma * y_std, y_center < y_mean - sigma * y_std)
+    quality = np.logical_or(x_cond, y_cond)
+
 def make_quality_flag(sector, sigma=2.):
     #セクター中のすべてのqualityを統合
     quality_list = []
     print("caliculating quality...")
     for came, chi in product("1234", "1234"):
+        #FFIを取得
         fitslist = loadFFI(sector, came, chi)
-        x_center = np.array([])
-        y_center = np.array([])
-        print("camera%s chip%s..." % (came, chi))
-        for fitspath in tqdm(fitslist):
-            #x, y座標を格納
-            with fits.open(fitspath) as hdu:
-                x = hdu[1].header["CRVAL1"]
-                y = hdu[1].header["CRVAL2"]
-                x_center = np.hstack((x_center, x))
-                y_center = np.hstack((y_center, y))
-        #統計値を算出
-        x_mean = np.nanmean(x_center)
-        y_mean = np.nanmean(y_center)
-        x_std = np.nanstd(x_center)
-        y_std = np.nanstd(y_center)
-        #sigma以上離れている点を1とする
-        x_cond = np.logical_or(x_center > x_mean + sigma * x_std, x_center < x_mean - sigma * x_std)
-        y_cond = np.logical_or(y_center > y_mean + sigma * y_std, y_center < y_mean - sigma * y_std)
-        quality = np.logical_or(x_cond, y_cond)
+        #x, yの位置の時系列データを取得
+        x_center, y_center = fits2pos(fitslist)
+        #x, yの位置の時系列データからqualityを算出
+        quality = pos2quality(x_center, y_center, sigma)
         quality_list.append(quality)
+    #qualityが少なくとも1つ以上1であったらフラグを立てる
     quality_arr = np.sum(np.vstack(quality_list), axis=0)
     quality_arr = np.where(quality_arr > 0, 1, 0)
+    #前後1点もqualityフラグを立てる
     quality_arr = binary_dilation(quality_arr)
     return quality_arr
-
-def gather_hdf(sector):
-    h5list = glob.glob(os.path.join(step2, "*_%s_?_?.h5" % sector))
-    return h5list
 
 def add_quality_flg(h5path, quality_arr):
     #path決める
