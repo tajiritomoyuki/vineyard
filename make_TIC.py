1c9858dd11f9621f2095f4a10387aea51398db1d
tajiri <tajiri_leader@nagasebros.com>
2019-03-09 10:01:47 +0900

    sector6 CTL

--- make_TIC.py
+++ make_TIC.py
@@ -86,7 +86,7 @@ def main():
     Tmag_limit = 13
     TICresult = get_TIC(Tmag_limit)
     # CTLdf = get_CTL(Tmag_limit)
-    for sector, camera, chip in product("12345", "1234", "1234"):
+    for sector, camera, chip in product("6", "1234", "1234"):
         regi = Register(sector, camera, chip)
         regi.get_wcs()
         regi.register(TICresult)
--- quality.py
+++ quality.py
@@ -110,7 +110,6 @@ def add_quality_flg(h5path, quality_arr):
 
 def main():
     #各セクターごとにクオリティフラグを作成
-<<<<<<< HEAD
     # for sector in range(1, 6):
     sector = 6
     quality_arr = make_quality_flag(sector)
@@ -118,14 +117,6 @@ def main():
     h5list = gather_hdf(sector)
     #qualityを付与
     Parallel(n_jobs=32)(delayed(add_quality_flg)(h5path, quality_arr) for h5path in tqdm(h5list))
-=======
-    for sector in range(1, 6):
-        quality_arr = make_quality_flag(sector)
-        #hdf集める
-        h5list = gather_hdf(sector)
-        #qualityを付与
-        Parallel(n_jobs=16)(delayed(add_quality_flg)(h5path, quality_arr) for h5path in tqdm(h5list))
->>>>>>> 4e4f59073d3467c6f5b2433cf7bab9073ea478f7
 
 if __name__ == '__main__':
     main()
