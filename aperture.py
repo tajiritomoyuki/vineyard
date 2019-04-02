2c1f21691e584f633dbc6a3f98bcc14926a835d3
tajiri <tajiri_leader@nagasebros.com>
2019-04-02 11:37:40 +0900

    TIC

--- aperture.py
+++ aperture.py
@@ -14,7 +14,7 @@ from astroquery.mast import Catalogs
 import matplotlib.pyplot as plt
 
 
-pattern = "CTL"
+pattern = "TIC"
 
 if pattern == "CTL":
     step1 = "/pike/pipeline/step1"
--- quality.py
+++ quality.py
@@ -13,7 +13,7 @@ from astropy.io import fits
 from astropy.coordinates import SkyCoord
 from astropy.wcs import WCS
 
-pattern = "CTL"
+pattern = "TIC"
 
 FFIdir = "/stingray/tess/data/FFI"
 
