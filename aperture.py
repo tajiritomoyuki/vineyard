025b09f356c87f2c9b95040013ab2a47c9f79853
tajiri <tajiri_leader@nagasebros.com>
2019-04-04 15:23:35 +0900

    all camera

--- aperture.py
+++ aperture.py
@@ -161,5 +161,5 @@ def main(h5path):
 if __name__ == '__main__':
     #ファイルリストを取得
     sector = 7
-    h5list = h5list = glob.glob(os.path.join(step1, "*_%s_1_?.h5" % sector))
-    Parallel(n_jobs=20)(delayed(main)(h5path) for h5path in tqdm(h5list))
+    h5list = h5list = glob.glob(os.path.join(step1, "*_%s_?_?.h5" % sector))
+    Parallel(n_jobs=30)(delayed(main)(h5path) for h5path in tqdm(h5list))
