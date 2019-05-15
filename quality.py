55660c48fa03b516b4db909628d50e767c2ef862
tajiritomoyuki <constantinoplite@gmail.com>
2019-05-15 15:35:52 +0900

    quality

--- quality.py
+++ quality.py
@@ -124,7 +124,7 @@ def main():
     #hdf集める
     h5list = gather_hdf(sector)
     #qualityを付与
-    Parallel(n_jobs=32)(delayed(add_quality_flg)(h5path, quality_arr) for h5path in tqdm(h5list))
+    Parallel(n_jobs=30)(delayed(add_quality_flg)(h5path, quality_arr) for h5path in tqdm(h5list))
 
 if __name__ == '__main__':
     main()
